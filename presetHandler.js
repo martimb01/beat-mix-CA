92ea4f5007601788d4ab1ccaedb2a69821bd6923
martimb01 <85357387+martimb01@users.noreply.github.com>
2024-12-04 17:42:17 +0000

    Added the presetHandler()

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,24 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method,indexArray, newPresetArray) => {
+    if (indexArray<0 || indexArray>15 ) {
+
+        return [404];
+
+    } else if (method != 'PUT' &&  method != 'GET') {
+
+        return [400];
+
+    } else {
+        if (method === 'GET') {
+            return [200,presets[indexArray]]
+        } else if (method === 'PUT') {
+            presets[indexArray] = newPresetArray;
+            return [200,newPresetArray];
+        }
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
