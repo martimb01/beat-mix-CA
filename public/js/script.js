d92701dbe0b901e9ee80d6cd93251e25ec5a1f1c
martimb01 <85357387+martimb01@users.noreply.github.com>
2024-12-04 17:05:34 +0000

    Added all 4 functions to script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,57 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = new Array(16).fill(false);
+let snares = new Array(16).fill(false);
+let hiHats = new Array(16).fill(false);
+let rideCymbals = new Array(16).fill(false);
+
+toggleDrum = (arrayName, indexNumber) => {
+
+    if (indexNumber<0 || indexNumber>15) {
+        return console.log('IndexNumber invalid! Must be 0-15')
+    } else {
+        switch (arrayName) {
+            case 'kicks':
+                if (kicks[indexNumber] === true) {
+                    kicks[indexNumber] = false;
+                } else {kicks[indexNumber] = true}
+            break;
+            case 'snares':
+                if (snares[indexNumber] === true) {
+                    snares[indexNumber] = false;
+                } else {snares[indexNumber] = true}
+            break;
+            case 'hihats':
+                if (hiHats[indexNumber] === true) {
+                    hiHats[indexNumber] = false;
+                } else {hiHats[indexNumber] = true}
+            break;
+            case 'rideCymbals':
+                if (rideCymbals[indexNumber] === true) {
+                    rideCymbals[indexNumber] = false;
+                } else {rideCymbals[indexNumber] = true}
+            break;
+            default:
+                console-log('Array Name is invalid');
+             
+        }
+    }
+}
+
+clear = (arrayName) => {
+
+    return arrayName.fill(false);
+
+}
+
+invert = (arrayName) => {
+
+    for (i=0; i<arrayName.length; i++ ) {
+
+        if (arrayName[i] === true) {
+
+            arrayName[i] = false;
+
+        } else {arrayName[i] = true}
+    }
+}
 
